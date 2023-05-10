144036d24ea70bd6824c786e1336dc54f691b3aa
Auston M Hefling <auston.hefling@outlook.com>
2023-05-09 21:25:18 -0400

    added services component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,152 @@
 import React from "react";
 import "./services.css";
+import { BiCheck } from "react-icons/bi";
 
 const Services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services_container">
+        <article className="service">
+          <div className="service_head">
+            <h3>SERVICE 1</h3>
+          </div>
+
+          <ul className="service_list">
+            <li>
+              <BiCheck className="service_list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service_list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service_list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service_list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service_list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </li>
+          </ul>
+        </article>
+
+        {/* END OF CARD ONE */}
+
+        <article className="service">
+          <div className="service_head">
+            <h3>SERVICE 2</h3>
+          </div>
+
+          <ul className="service_list">
+            <li>
+              <BiCheck className="service_list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service_list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service_list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service_list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service_list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </li>
+          </ul>
+        </article>
+
+        {/* END OF CARD TWO */}
+
+        <article className="service">
+          <div className="service_head">
+            <h3>SERVICE 3</h3>
+          </div>
+
+          <ul className="service_list">
+            <li>
+              <BiCheck className="service_list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service_list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service_list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service_list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service_list_icon" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
