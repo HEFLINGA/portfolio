c2fdfe0b6dc5063052dfc0bb0a49aa33d074f06e
Auston M Hefling <auston.hefling@outlook.com>
2023-05-13 11:58:21 -0400

    Added projects with images and desc.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import "./portfolio.css";
 import IMG1 from "../../assets/portfolio_img.png";
 import IMG2 from "../../assets/bb_img.jpg";
+import IMG3 from "../../assets/vscode_cc_img.jpg";
 
 const data = [
   {
     id: 1,
     image: IMG1,
     title: "Personal Portfolio",
-    description: "My personal portfolio for displaying my current skills, experience, and fully completed projects, as well as to showcase my frontend development abilities.",
-    tech: "React, Tailwindcss, HTML/CSS",
+    description: "My personal portfolio for displaying my current skills, experience, and fully completed projects, as well as to showcase my frontend development abilities",
+    tech: "React, Tailwindcss, HTML/CSS, EmailJs",
     github: "https://github.com/HEFLINGA/ExerciseClassProject",
     demo: "https://heflinga.github.io/",
   },
@@ -17,9 +18,18 @@ const data = [
     id: 2,
     image: IMG2,
     title: "Microsoft Software and Systems Academy Final Project",
-    description: "Stuff",
-    tech: "thing 1, thing 2",
+    description: "The final project for a fullstack web application bootcamp with a focus on cloud application development put on by Microsoft and Embry-Riddle Aeronautical University",
+    tech: "ASP.NET, MVC, Agile/KanBan, JavaScript, C#, REST, Azure, Bootstrap",
     github: "https://github.com/HEFLINGA/ExerciseClassProject",
+    demo: "",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "VSCode Code Auto Completion Extension",
+    description: "An extension created for Los Alamos National Laboratory while working as a developer/research intern. This extension gives completion suggestions based on current context in Python scripts, using Huggingface LLM API",
+    tech: "TypeScript, HuggingFace LLM API, VSCode, Python",
+    github: "https://github.com/HEFLINGA/VSCodeExtension_CodeCompletion",
     demo: "",
   },
 ];
@@ -39,7 +49,7 @@ const Portfolio = () => {
               </div>
               <h3>{title}</h3>
               <p>{description}</p>
-              <p><strong>Technology used:</strong> {tech}</p>
+              <p><strong>Relevent Skills:</strong> {tech}</p>
               <div className="portfolio_item_cta">
                 <a
                   href={github}
